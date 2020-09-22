c0feb002ead9ae4380ea7d853371497547f01388
Chetan Bhoi <chetan.bhoi@einfochips.com>
2020-09-22 19:13:02 +0530

    [cbhoi] Added test data for sorting test cases

--- variable_files/tabulatorTestData.py
+++ variable_files/tabulatorTestData.py
@@ -38,3 +38,52 @@ filter_table_with_name_as_wildcardchars = {
             'rowCount': '0'
         }
 }
+sort_table_by_name_field = {
+        'sort':
+        {
+            'fieldName': 'name',
+            'sortType': 'asc'
+        }
+}
+sort_table_by_progress_field = {
+        'sort':
+        {
+            'fieldName': 'progress',
+            'sortType': 'asc'
+        }
+}
+sort_table_by_gender_field = {
+        'sort':
+        {
+            'fieldName': 'gender',
+            'sortType': 'asc'
+        }
+}
+sort_table_by_rating_field = {
+        'sort':
+        {
+            'fieldName': 'rating',
+            'sortType': 'asc'
+        }
+}
+sort_table_by_color_field = {
+        'sort':
+        {
+            'fieldName': 'col',
+            'sortType': 'asc'
+        }
+}
+sort_table_by_dob_field = {
+        'sort':
+        {
+            'fieldName': 'dob',
+            'sortType': 'asc'
+        }
+}
+sort_table_by_driver_field = {
+        'sort':
+        {
+            'fieldName': 'car',
+            'sortType': 'asc'
+        }
+}
