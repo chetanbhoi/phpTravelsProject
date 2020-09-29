9ab436ed5fb5127e4d3e418a6a11c422e9482e80
Chetan Bhoi <chetan.bhoi@einfochips.com>
2020-09-29 13:18:44 +0530

    [cbhoi] - added "get_csv_data_in_list" function

--- Common/common_functions.py
+++ Common/common_functions.py
@@ -1,4 +1,5 @@
 # Import the datetime module
+import csv
 from datetime import datetime
 
 def get_sorted_number(list):
@@ -14,4 +15,14 @@ def get_sorted_number(list):
 
 def get_sorted_dates(list):
     list.sort(key=lambda date: datetime.strptime(date, '%d/%m/%Y'))
+    return list
+
+def get_csv_data_in_list(filename):
+    with open(filename,"r")as csv_file:
+        csv_reader = csv.reader(csv_file)
+        list = []
+        for line in csv_reader:
+            print(line)
+            list.append(line)
+
     return list
